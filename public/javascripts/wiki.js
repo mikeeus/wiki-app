65fbe86a584879d301fba7ec63ba71f04bb39dc3
mikeeus <itsmikias@gmail.com>
2016-10-31 11:20:59 +0300

    Initial commit

---
+++ public/javascripts/wiki.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+  // Event handler for search bar
+  // On keyup, send ajax request to wikipedia api
+  // 
+  rivets.bind($('#search-results'), {searchResults: searchResults});
+
+});
+
+var searchResults = {
+  results: []
+};
+
+var search = _.debounce( function() {
+  input = $("#search-input")[0].value;
+  if(input.length > 2){
+    console.log(input);
+    $.ajax({
+      url: 'http://en.wikipedia.org/w/api.php',
+      data: { action: 'query', list: 'search', srsearch: input, format: 'json' },
+      dataType: 'jsonp',
+      success: function (res) {
+        console.log(res)
+        searchResults.results = res.query.search;
+      }
+    });
+  }
+}, 500);
+
+function goToLink(params){
+  console.log('params: ', params.text);
+  window.location.href = "http://en.wikipedia.org/wiki/" + params.text.trim();
+}
+
+// rivets
+rivets.configure({
+  // Attribute prefix in templates
+  prefix: 'rv',
+  // Preload templates with initial data on bind
+  preloadData: true,
+  // Root sightglass interface for keypaths
+  rootInterface: '.',
+  // Template delimiters for text bindings
+  templateDelimiters: ['{', '}'],
+  // Alias for index in rv-each binder
+  iterationAlias : function(modelName) {
+    return '%' + modelName + '%';
+  },
+  // Augment the event handler of the on-* binder
+  handler: function(target, event, binding) {
+    this.call(target, event, binding.view.models)
+  },
+  // Since rivets 0.9 functions are not automatically executed in expressions. If you need backward compatibilty, set this parameter to true
+  executeFunctions: false
+});
